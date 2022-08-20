66bc79e0993bb1269e3938a077c536669176276b
rg <rgrg@cisco.com>
2022-08-20 12:48:03 +0530

    Seperated as singleCoin comp + scrolling working

---
+++ components/CoinsComponent.tsx
@@ -0,0 +1,67 @@
+import { View, Text, Image, StyleSheet } from 'react-native'
+import React from 'react'
+import { CoinType } from "../types";
+
+
+export const CoinsComponent = ({coin}) => {
+  return (
+        <View style={styles.singleCoin}>
+            <Image style={styles.logoImage} source={{uri: coin.logo_url}}></Image>
+            <View style={styles.coinNameSymbolContainer}>
+                <Text style={styles.coinName} >{coin.name}</Text>
+                <Text style={styles.coinSymbol} >{coin.symbol}</Text>
+            </View>
+            <View style={styles.coinPriceContainer}>
+                <Text style={styles.coinTotal} >${coin.total_value}</Text>
+                <Text style={styles.coinPrice} >{coin.price.toFixed(2)}</Text>
+            </View>
+        </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    singleCoin: {
+        flexDirection: 'row',
+        backgroundColor: "white",
+        padding: "10px",
+        borderBottomColor: "lightgray",
+        borderBottomWidth: 1,
+        borderTopWidth: 1,
+        borderTopColor: "lightgray",
+    },
+    logoImage:{
+        height: "50px",
+        width: "50px",
+        borderRadius: 100,
+        
+    },
+    coinName:{
+        color: "black",
+        padding: "10px",
+        fontWeight: 'bold',
+        paddingLeft: '16px'
+    },
+    coinSymbol:{
+        color: "gray",
+        paddingLeft: "16px",
+    },
+    coinNameSymbolContainer:{
+        
+    },
+    coinPriceContainer:{
+        flex:1,
+        justifyContent: "space-between",
+        right: 0,
+        // position: 'absolute',
+        marginRight: "10px"
+    },
+    coinTotal:{
+        textAlign: "right",
+        fontSize: 13,
+        fontWeight: "bold"
+    },
+    coinPrice:{
+        textAlign: "right",
+        color: "#b1b1b1",
+    }
+})
--- screens/Portfolio.tsx
+++ screens/Portfolio.tsx
@@ -1,42 +1,66 @@
-import { View, Text, StyleSheet, Dimensions, ScrollView, Image } from 'react-native'
+import { View, Text, StyleSheet, Dimensions, ScrollView, Image, SafeAreaView } from 'react-native'
 import { Checkbox } from 'react-native-paper';
 import React, { useEffect, useState } from 'react'
 import { get_coins } from "../services/apis";
 import { CoinType } from "../types";
+import { CoinsComponent } from '../components/CoinsComponent';
+import { LoadWalletComponent } from '../components/LoadWalletComponent';
 const windowWidth = Dimensions.get("window").width;
 
 const Portfolio = () => {
 
     const [Coins, setCoins] = useState<CoinType[]>([])
     const [verified, setVerified] = useState(true)
+    const [coinChains, setcoinChains] = useState([])
 
-    const update_coins = () => {
-        get_coins().then(
-            (response) => {
-                let chain_portfolios = response.data.record.chain_portfolios
-                let coins_array: CoinType[] = [];
-
-                for (let chain of chain_portfolios) {
-                    for (let coin of chain.token_holdings) {
-                        if (coin.is_verified === verified)
-                            coins_array.push(coin)
-                    }
-                }
-                setCoins(coins_array)
+    const update_coins = (coinChainsArr) => {
+        let coins_array: CoinType[] = [];
+        for (let chain of coinChainsArr) {
+            for (let coin of chain.token_holdings) {
+                if (coin.is_verified === verified)
+                    coins_array.push(coin)
             }
-        )
+        }
+        console.log(coins_array)
+        setCoins(coins_array)
+    }
+
+    const load_coins = () => {
+        get_coins().then(
+                (response) => {
+                    setcoinChains(response.data.record.chain_portfolios)
+                    update_coins(response.data.record.chain_portfolios)
+                }
+            )
     }
 
     useEffect(() => {
-        update_coins()
+        load_coins()
     }, [])
 
     useEffect(() => {
-        update_coins()
+        update_coins(coinChains)
     }, [verified])
 
     return (
         <View>
+<View style={styles.middleContainer}>
+                <View style={styles.lastUpdatedContainer}>
+                    <Text>Last Updated: 4 Mins Ago</Text>
+                </View>
+                <View style={styles.checkboxContainer}>
+                    <Checkbox
+                        status={verified ? 'checked' : 'unchecked'}
+                        onPress={() => { setVerified(!verified) }}
+                        color="black"
+                    />
+                    <Text style={styles.verifiedCoinText}>Only Verified Coins</Text>
+                </View>
+            </View>
+
+
+
+
             <View style={styles.middleContainer}>
                 <View style={styles.lastUpdatedContainer}>
                     <Text>Last Updated: 4 Mins Ago</Text>
@@ -50,20 +74,14 @@ const Portfolio = () => {
                     <Text style={styles.verifiedCoinText}>Only Verified Coins</Text>
                 </View>
             </View>
-            <View style={styles.coinsContainer}>
-                {Coins.map((coin, index) => (
-                    <View key={index} style={styles.singleCoin}>
-                        <Image style={styles.logoImage} source={{uri: coin.logo_url}}></Image>
-                        <View style={styles.coinNameSymbolContainer}>
-                            <Text style={styles.coinName} >{coin.name}</Text>
-                            <Text style={styles.coinSymbol} >{coin.symbol}</Text>
-                        </View>
-                        <View style={styles.coinPriceContainer}>
-                            <Text style={styles.coinTotal} >{coin.total_value}</Text>
-                            <Text style={styles.coinPrice} >{coin.price.toFixed(2)}</Text>
-                        </View>
-                    </View>
-                ))}
+            <View style={{height: "77.5vh"}}>
+                <View style={styles.coinsContainer}>
+                {
+                    Coins.map((coin, index) => (
+                        <CoinsComponent key={index} coin={coin}/>
+                    ))
+                }
+                </View>
             </View>
         </View>
     )
@@ -107,51 +125,10 @@ const styles = StyleSheet.create({
     coinsContainer: {
         flex: 1,
         flexDirection: 'column',
-        justifyContent: "space-between",
+        // justifyContent: "space-between",
         overflow: "scroll",
-        bottom:"90px"
-    },
-    singleCoin: {
-        flexDirection: 'row',
-        backgroundColor: "white",
-        padding: "10px",
-        borderBottomColor: "lightgray",
-        borderBottomWidth: 1,
-        borderTopWidth: 1,
-        borderTopColor: "lightgray",
-    },
-    logoImage:{
-        height: "50px",
-        width: "50px",
-        borderRadius: 100,
-        
-    },
-    coinName:{
-        color: "black",
-        padding: "10px",
-        fontWeight: 'bold'
-    },
-    coinSymbol:{
-        color: "gray",
-        paddingLeft: "10px"
-    },
-    coinNameSymbolContainer:{
-        
-    },
-    coinPriceContainer:{
-        flex:1,
-        justifyContent: "space-between",
-        right: 0,
-        // position: 'absolute',
-        marginRight: "10px"
-    },
-    coinTotal:{
-        textAlign: "right",
-        fontSize: 13,
-        color: "#b1b1b1",
-        fontWeight: "bold"
-    },
-    coinPrice:{
-        textAlign: "right",
+        flexGrow: 1 ,
+        // padding: "10px"
+        // bottom:"90px"
     }
 })
